e0b726f40a2f7a76a10f59a64086db425d940bab
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2018-08-10 10:46:51 -0500

    Finished angular ng-repeat for contributors

---
+++ README.md
@@ -0,0 +1,19 @@
+## Personal website
+
+This website was intended to be a class project written in HTML and vanilla JS but we decided to turn it into a learning project for various front-end technologies. What we are doing right now is learning MEAN, MERN stack. We might choose to include M in MEAN in the future given the time.
+
+Contributors:
+ - Keith Nguyen 
+ - Cynthia Nguyen 
+ - Peter Tran 
+ - Hung Vuong
+
+To run:
+
+ 1. Clone the repository
+ 2. cd into the folder
+ 3. `npm i`
+ 4. Verify that server is running and go to `localhost:3000`
+ 5. Check out `localhost:about` or `localhost:3000/contributor`
+
+
--- public/javascripts/contributorRouting.js
+++ public/javascripts/contributorRouting.js
@@ -4,10 +4,37 @@ app.controller('contributorController', function ($scope) {
   $scope.member = ["Khoa T Nguyen", "Hung Vuong", "Cynthia Nguyen", "Peter Tran"];
   $scope.members = [{
     name: "Khoa T Nguyen",
-    github: "khoanguyen0791"
+    github: "https://github.com/khoanguyen0791",
+    email: "kxn161730@utdallas.edu",
+    facebook: "https://www.facebook.com/ktn197",
+    linkedin: "https://www.linkedin.com/in/ktn0047/",
+    phone: "660-988-3837",
+    biography: `Keith Nguyen is pursuing a Computer Science degree at UT Dallas.
+    Previously,
+    he worked at Associa and used his coding skills to automate invoice printing,
+    journal posting and data retrieval from company database. His personal projects
+    involves contributing to ally-api tool, pitest and Express, Angular and ReactJS.
+    `
+
   }, {
-    name: "Khoa T Nguyen",
-    github: "khoanguyen0791"
+    name: "Hung Vuong",
+    github: "https://github.com/hungvuong816/",
+    email: "hungvuong816@gmail.com",
+    phone: "469-358-9713"
+  }, {
+    name: "Cynthia Nguyen",
+    github: "https://github.com/hungvuong816/",
+    email: "",
+    phone: "",
+    github: "https://github.com/anhcynthia",
+    facebook: "https://www.facebook.com/AnhCynthiaNguyen94"
+  }, {
+    name: "Peter Tran",
+    github: "",
+    email: "",
+    phone: "469-693-0637",
+    github: "",
+    facebook: ""
   }]
 });
 alert("I'm trying to do dynamic loading with ngRoute, but EJS prevents serving static webpages.");
